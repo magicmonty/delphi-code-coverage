ba26698eec615f70b0720a4e62dc09bf04392396
christer.fahlgren <christer.fahlgren@b99ad5fc-3a95-0f71-d2d7-ed0b564f6493>
2010-07-02 21:46:29 +0000

    Cleaning up xml format
    git-svn-id: http://delphi-code-coverage.googlecode.com/svn/trunk@16 b99ad5fc-3a95-0f71-d2d7-ed0b564f6493

--- CodeCoverage/XMLCoverageReport.pas
+++ CodeCoverage/XMLCoverageReport.pas
@@ -19,15 +19,11 @@ implementation
 uses sysutils, strutils, jclfileutils;
 
 procedure TXMLCoverageReport.generate(coverage: TCoverage; sourcedir: string; outputdir: string);
-var
-  i: Integer;
 begin
   generateSummaryReport(coverage, outputdir);
-
 end;
 
 procedure TXMLCoverageReport.generateSummaryReport(coverage: TCoverage; outputdir: string);
-
 var
   OutputFile: TextFile;
   i: Integer;
@@ -42,37 +38,37 @@ begin
     AddPreAmble(OutputFile);
 
     writeln(OutputFile, '<stats>');
-    writeln(OutputFile, '<packages value="' + inttostr(coverage.getUnitCount()) + '"/>');
-    writeln(OutputFile, '<classes value="0"/>');
-    writeln(OutputFile, '<methods value="0"/>');
-    writeln(OutputFile, '<srcfiles value="' + inttostr(coverage.getUnitCount()) + '"/>');
-    writeln(OutputFile, '<srclines value="' + inttostr(coverage.GetNumberOfLines()) + '"/>');
+    writeln(OutputFile, '  <packages value="' + inttostr(coverage.getUnitCount()) + '"/>');
+    writeln(OutputFile, '  <classes value="0"/>');
+    writeln(OutputFile, '  <methods value="0"/>');
+    writeln(OutputFile, '  <srcfiles value="' + inttostr(coverage.getUnitCount()) + '"/>');
+    writeln(OutputFile, '  <srclines value="' + inttostr(coverage.GetNumberOfLines()) + '"/>');
     writeln(OutputFile, '</stats>');
 
     writeln(OutputFile, '<data>');
-    writeln(OutputFile, '<all name="all classes">');
+    writeln(OutputFile, '  <all name="all classes">');
 
     for i := 0 to coverage.getUnitCount - 1 do
     begin
-      writeln(OutputFile, ' <package name="' + coverage.CoverageUnit[i].GetName + '">');
-      writeln(OutputFile, '<coverage type="class, %" value="0% (0/0)"/>');
-      writeln(OutputFile, '<coverage type="method, %" value="0% (0/0)"/>');
-      writeln(OutputFile, '<coverage type="block, %" value="0% (0/0)"/>');
-        writeln(OutputFile, '<coverage type="line, %" value="' + inttostr(coverage.CoverageUnit[i].GetPercentCovered())
+      writeln(OutputFile, '    <package name="' + coverage.CoverageUnit[i].GetName + '">');
+      writeln(OutputFile, '      <coverage type="class, %" value="0% (0/0)"/>');
+      writeln(OutputFile, '      <coverage type="method, %" value="0% (0/0)"/>');
+      writeln(OutputFile, '      <coverage type="block, %" value="0% (0/0)"/>');
+      writeln(OutputFile, '      <coverage type="line, %" value="' + inttostr(coverage.CoverageUnit[i].GetPercentCovered())
           + '% (' + inttostr(coverage.CoverageUnit[i].GetNumberOfCoveredLines()) + '/' + inttostr
           (coverage.CoverageUnit[i].GetNumberOfLines()) + ')"/>');
 
-      writeln(OutputFile, ' <srcfile name="' + coverage.CoverageUnit[i].GetName + '.pas">');
-      writeln(OutputFile, '<coverage type="class, %" value="0% (0/0)"/>');
-      writeln(OutputFile, '<coverage type="method, %" value="0% (0/0)"/>');
-      writeln(OutputFile, '<coverage type="block, %" value="0% (0/0)"/>');
-      writeln(OutputFile, '<coverage type="line, %" value="' + inttostr(coverage.CoverageUnit[i].GetPercentCovered())
+      writeln(OutputFile, '      <srcfile name="' + coverage.CoverageUnit[i].GetName + '.pas">');
+      writeln(OutputFile, '        <coverage type="class, %" value="0% (0/0)"/>');
+      writeln(OutputFile, '        <coverage type="method, %" value="0% (0/0)"/>');
+      writeln(OutputFile, '        <coverage type="block, %" value="0% (0/0)"/>');
+      writeln(OutputFile, '        <coverage type="line, %" value="' + inttostr(coverage.CoverageUnit[i].GetPercentCovered())
           + '% (' + inttostr(coverage.CoverageUnit[i].GetNumberOfCoveredLines()) + '/' + inttostr
           (coverage.CoverageUnit[i].GetNumberOfLines()) + ')"/>');
-      writeln(OutputFile, ' </srcfile>');
-      writeln(OutputFile, ' </package>');
+      writeln(OutputFile, '      </srcfile>');
+      writeln(OutputFile, '    </package>');
     end;
-    writeln(OutputFile, '</all>');
+    writeln(OutputFile, '  </all>');
     writeln(OutputFile, '</data>');
     AddPostAmble(OutputFile);
     CloseFile(OutputFile);
