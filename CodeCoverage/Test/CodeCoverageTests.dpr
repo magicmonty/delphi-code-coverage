b1bbb6b5a2c499e984d2aa39dcde780a74623975
christer.fahlgren <christer.fahlgren@b99ad5fc-3a95-0f71-d2d7-ed0b564f6493>
2010-07-02 00:12:08 +0000

    Adding first unit tests
    git-svn-id: http://delphi-code-coverage.googlecode.com/svn/trunk@10 b99ad5fc-3a95-0f71-d2d7-ed0b564f6493

---
+++ CodeCoverage/Test/CodeCoverageTests.dpr
@@ -0,0 +1,35 @@
+program CodeCoverageTests;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  TestConfiguration in 'TestConfiguration.pas',
+  Configuration in '..\Configuration.pas';
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+    with TextTestRunner.RunRegisteredTests do
+      Free
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
