5efc1517159e2d8a8d44432f8293417125ed05fe
christer.fahlgren <christer.fahlgren@b99ad5fc-3a95-0f71-d2d7-ed0b564f6493>
2010-08-29 05:34:34 +0000

    git-svn-id: http://delphi-code-coverage.googlecode.com/svn/trunk@27 b99ad5fc-3a95-0f71-d2d7-ed0b564f6493

--- build.bat
+++ build.bat
@@ -5,5 +5,5 @@ echo %PATH%
 
 call "SetupEnvironment.Bat"
 
-msbuild  /p:Win32LibraryPath="$(BDS)\lib;$(BDS)\include;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Win32API;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Common;C:\lib\jcl\jcl\source\include;C:\lib\jcl\jcl\lib\d14;" /p:IncludePath="C:\lib\fastmm;C:\lib\jcl\jcl\source\include /p:UnitSearchPath="C:\lib\FastMM" /t:build /p:config=Debug /verbosity:detailed "CodeCoverage\CodeCoverage.dproj"
+msbuild  /p:Win32LibraryPath="$(BDS)\lib;$(BDS)\include;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Win32API;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Common;C:\lib\jcl\jcl\source\include;C:\lib\jcl\jcl\lib\d14;" /p:IncludePath="C:\lib\fastmm;C:\lib\jcl\jcl\source\include /p:UnitSearchPath="$(UnitSearchPath);C:\lib\FastMM" /t:build /p:config=Debug /verbosity:detailed "CodeCoverage\CodeCoverage.dproj"
 
