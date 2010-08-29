214934ea094d5fd80ac22d6cdf2c201286aeae2d
christer.fahlgren <christer.fahlgren@b99ad5fc-3a95-0f71-d2d7-ed0b564f6493>
2010-08-29 05:13:24 +0000

    Include FastMM4
    git-svn-id: http://delphi-code-coverage.googlecode.com/svn/trunk@22 b99ad5fc-3a95-0f71-d2d7-ed0b564f6493

--- build.bat
+++ build.bat
@@ -5,6 +5,6 @@ echo %PATH%
 
 call "SetupEnvironment.Bat"
 
-msbuild  /p:Win32LibraryPath="$(BDS)\lib;$(BDS)\include;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Win32API;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Common;C:\lib\jcl\jcl\source\include;C:\lib\jcl\jcl\lib\d14;" /t:build /p:config=Debug /verbosity:detailed "CodeCoverage\CodeCoverage.dproj"
+msbuild  /p:Win32LibraryPath="$(BDS)\lib;$(BDS)\include;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Win32API;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Common;C:\lib\jcl\jcl\source\include;C:\lib\jcl\jcl\lib\d14;" /p:IncludePath=C:\lib\fastmm; /t:build /p:config=Debug /verbosity:detailed "CodeCoverage\CodeCoverage.dproj"
 
 
