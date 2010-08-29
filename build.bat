e2ed6b33df8a2f10bb9716e6f3096b7c50af0485
christer.fahlgren <christer.fahlgren@b99ad5fc-3a95-0f71-d2d7-ed0b564f6493>
2010-08-29 06:07:39 +0000

    git-svn-id: http://delphi-code-coverage.googlecode.com/svn/trunk@31 b99ad5fc-3a95-0f71-d2d7-ed0b564f6493

--- build.bat
+++ build.bat
@@ -5,5 +5,5 @@ echo %PATH%
 
 call "SetupEnvironment.Bat" 
 
-msbuild  /p:Win32LibraryPath="$(BDS)\lib;$(BDS)\include;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Win32API;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Common;C:\lib\jcl\jcl\source\include;C:\lib\jcl\jcl\lib\d14;C:\lib\FastMM;C:\lib\jcl\jcl\source\include"  /t:build /p:config=Debug /verbosity:detailed "CodeCoverage\CodeCoverage.dproj"
+msbuild  /p:Win32LibraryPath="$(BDS)\lib;$(BDS)\include;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Win32API;C:\Users\christer\Downloads\jedi_api22a_jwscl092a\jwapi2.2a\Common;C:\lib\jcl\jcl\source\include;C:\lib\jcl\jcl\lib\d14;C:\lib\FastMM;C:\lib\jcl\jcl\source\include;C:\lib\Adom_5_1\sources;C:\lib\Utilities\sources;"  /t:build /p:config=Debug /verbosity:detailed "CodeCoverage\CodeCoverage.dproj"
 
