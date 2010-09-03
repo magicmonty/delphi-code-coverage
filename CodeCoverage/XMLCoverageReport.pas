e7931468eda7536c8842ff1f758c578c52b90ffd
nring@southcom.com.au <nring@southcom.com.au@b99ad5fc-3a95-0f71-d2d7-ed0b564f6493>
2010-09-03 08:41:55 +0000

    Fix: Issue 14:Coverage XML report not quite EMMA compliant for CruiseControl.NET
    git-svn-id: http://delphi-code-coverage.googlecode.com/svn/trunk@37 b99ad5fc-3a95-0f71-d2d7-ed0b564f6493

--- CodeCoverage/XMLCoverageReport.pas
+++ CodeCoverage/XMLCoverageReport.pas
@@ -22,8 +22,8 @@ type
   private
     procedure AddPreAmble(const AOutFile: TextFile);
     procedure AddPostAmble(const AOutFile: TextFile);
-    function FormatPercentage(const ACoverageBase : ICoverageStats) : string;
-    //procedure GenerateSummaryReport(const ACoverage: ICoverage; const AOutputDir: string);
+    function FormatLinePercentage(const ACoverageStats : ICoverageStats) : string;
+    procedure WriteStats(const ACoverageStats : ICoverageStats; const AOutFile: TextFile; const AIndentCount : Integer);
   public
     procedure Generate(const ACoverage: ICoverageStats; const ASourceDir, AOutputDir: string);
   end;
@@ -39,10 +39,10 @@ procedure TXMLCoverageReport.Generate(const ACoverage: ICoverageStats; const
 var
   OutputFileName  : string;
   OutputFile      : TextFile;
-  lpUnit          : Integer;
   lpModule        : Integer;
-  CoverageModule  : ICoverageStats;
+  lpUnit          : Integer;
   CoverageUnit    : ICoverageStats;
+  CoverageModule  : ICoverageStats;
   SourceFileCount : Integer;
 begin
   //GenerateSummaryReport(ACoverage, AOutputDir);
@@ -62,8 +62,8 @@ begin
       WriteLn(OutputFile, '  <methods value="0"/>');
 
       SourceFileCount     := 0;
-      for lpUnit := 0 to Pred(ACoverage.GetCount) do
-        SourceFileCount := SourceFileCount + ACoverage.CoverageReport[lpUnit].GetCount;
+      for lpModule := 0 to Pred(ACoverage.GetCount) do
+        SourceFileCount := SourceFileCount + ACoverage.CoverageReport[lpModule].GetCount;
 
       WriteLn(OutputFile, '  <srcfiles value="' + IntToStr(SourceFileCount) + '"/>');
       WriteLn(OutputFile, '  <srclines value="' + IntToStr(ACoverage.GetNumberOfLines()) + '"/>');
@@ -72,25 +72,21 @@ begin
       WriteLn(OutputFile, '<data>');
       WriteLn(OutputFile, '  <all name="all classes">');
 
-      for lpUnit := 0 to Pred(ACoverage.GetCount) do
-      begin
-        CoverageUnit := ACoverage.CoverageReport[lpUnit];
-        WriteLn(OutputFile, '');
-        WriteLn(OutputFile, '    <package name="' + CoverageUnit.GetName + '">');
-        WriteLn(OutputFile, '      <coverage type="class, %" value="0%   (0/0)"/>');
-        WriteLn(OutputFile, '      <coverage type="method, %" value="0%   (0/0)"/>');
-        WriteLn(OutputFile, '      <coverage type="block, %" value="0%   (0/0)"/>');
-        WriteLn(OutputFile, '      <coverage type="line, %" value="' + FormatPercentage(CoverageUnit) + '"/>');
+      WriteStats(ACoverage, OutputFile, 0);
 
-        for lpModule := 0 to Pred(CoverageUnit.GetCount) do
+      for lpModule := 0 to Pred(ACoverage.GetCount) do
+      begin
+        CoverageModule := ACoverage.CoverageReport[lpModule];
+        WriteLn(OutputFile, '');
+        WriteLn(OutputFile, '    <package name="' + CoverageModule.GetName + '">');
+        WriteStats(CoverageModule, OutputFile, 1);
+
+        for lpUnit := 0 to Pred(CoverageModule.GetCount) do
         begin
-          CoverageModule := CoverageUnit.CoverageReport[lpModule];
+          CoverageUnit := CoverageModule.CoverageReport[lpUnit];
           WriteLn(OutputFile, '');
-          WriteLn(OutputFile, '      <srcfile name="' + CoverageModule.GetName() + '">');
-          WriteLn(OutputFile, '        <coverage type="class, %" value="0%   (0/0)"/>');
-          WriteLn(OutputFile, '        <coverage type="method, %" value="0%   (0/0)"/>');
-          WriteLn(OutputFile, '        <coverage type="block, %" value="0%   (0/0)"/>');
-          WriteLn(OutputFile, '        <coverage type="line, %" value="' + FormatPercentage(CoverageModule) + '"/>');
+          WriteLn(OutputFile, '      <srcfile name="' + CoverageUnit.GetName() + '">');
+          WriteStats(CoverageUnit, OutputFile, 2);
           WriteLn(OutputFile, '      </srcfile>');
         end;
         WriteLn(OutputFile, '    </package>');
@@ -107,81 +103,38 @@ begin
   end;
 end;
 
-//procedure TXMLCoverageReport.GenerateSummaryReport(const ACoverage: ICoverage;
-//    const AOutputDir: string);
-//var
-//  OutputFile: TextFile;
-//  lp: Integer;
-//  OutputFileName: string;
-//begin
-//  try
-//    OutputFileName := 'CodeCoverage_summary.xml';
-//    if (AOutputDir <> '') then
-//      OutputFileName := PathAppend(AOutputDir, OutputFileName);
-//    AssignFile(OutputFile, OutputFileName);
-//    rewrite(OutputFile);
-//    AddPreAmble(OutputFile);
-//
-//    WriteLn(OutputFile, '<stats>');
-//    WriteLn(OutputFile, '  <packages value="' + IntToStr(ACoverage.getUnitCount()) + '"/>');
-//    WriteLn(OutputFile, '  <classes value="0"/>');
-//    WriteLn(OutputFile, '  <methods value="0"/>');
-//    WriteLn(OutputFile, '  <srcfiles value="' + IntToStr(ACoverage.getUnitCount()) + '"/>');
-//    WriteLn(OutputFile, '  <srclines value="' + IntToStr(ACoverage.GetNumberOfLines()) + '"/>');
-//    WriteLn(OutputFile, '</stats>');
-//
-//    WriteLn(OutputFile, '<data>');
-//    WriteLn(OutputFile, '  <all name="all classes">');
-//
-//    for lp := 0 to ACoverage.getUnitCount - 1 do
-//    begin
-//      WriteLn(OutputFile, '    <package name="' + ACoverage.CoverageModule[lp].GetModuleName + '">');
-//      WriteLn(OutputFile, '      <coverage type="class, %" value="0% (0/0)"/>');
-//      WriteLn(OutputFile, '      <coverage type="method, %" value="0% (0/0)"/>');
-//      WriteLn(OutputFile, '      <coverage type="block, %" value="0% (0/0)"/>');
-//      WriteLn(OutputFile, '      <coverage type="line, %" value="' + IntToStr(ACoverage.CoverageModule[lp].GetPercentCovered())
-//          + '% (' + IntToStr(ACoverage.CoverageModule[lp].GetNumberOfCoveredLines()) + '/' + IntToStr
-//          (ACoverage.CoverageModule[lp].GetNumberOfLines()) + ')"/>');
-//
-//      WriteLn(OutputFile, '      <srcfile name="' + ACoverage.CoverageModule[lp].GetModuleName + '.pas">');
-//      WriteLn(OutputFile, '        <coverage type="class, %" value="0% (0/0)"/>');
-//      WriteLn(OutputFile, '        <coverage type="method, %" value="0% (0/0)"/>');
-//      WriteLn(OutputFile, '        <coverage type="block, %" value="0% (0/0)"/>');
-//      WriteLn(OutputFile, '        <coverage type="line, %" value="' + IntToStr(ACoverage.CoverageModule[lp].GetPercentCovered())
-//          + '% (' + IntToStr(ACoverage.CoverageModule[lp].GetNumberOfCoveredLines()) + '/' + IntToStr
-//          (ACoverage.CoverageModule[lp].GetNumberOfLines()) + ')"/>');
-//      WriteLn(OutputFile, '      </srcfile>');
-//      WriteLn(OutputFile, '    </package>');
-//    end;
-//    WriteLn(OutputFile, '  </all>');
-//    WriteLn(OutputFile, '</data>');
-//    AddPostAmble(OutputFile);
-//    CloseFile(OutputFile);
-//  except
-//    on E: EInOutError do
-//      WriteLn('Exception during generation of xml report - exception:' + E.message);
-//  end;
-//end;
+procedure TXMLCoverageReport.WriteStats(const ACoverageStats: ICoverageStats; 
+                                        const AOutFile: TextFile;
+                                        const AIndentCount: Integer);
+var
+  Padding : string;
+begin
+  Padding := '    ' + StringOfChar(' ', AIndentCount shl 1);
+  WriteLn(AOutFile, Padding + '<coverage type="class, %" value="0%   (0/0)"/>');
+  WriteLn(AOutFile, Padding + '<coverage type="method, %" value="0%   (0/0)"/>');
+  WriteLn(AOutFile, Padding + '<coverage type="block, %" value="0%   (0/0)"/>');
+  WriteLn(AOutFile, Padding + '<coverage type="line, %" value="' + FormatLinePercentage(ACoverageStats) + '"/>');
+end;
+
 
 procedure TXMLCoverageReport.AddPreAmble(const AOutFile: TextFile);
 begin
   WriteLn(AOutFile, '<report>');
 end;
 
-function TXMLCoverageReport.FormatPercentage(
-  const ACoverageBase: ICoverageStats): string;
+function TXMLCoverageReport.FormatLinePercentage(const ACoverageStats: ICoverageStats): string;
 var
   PercentageStr : string;
 begin
-  PercentageStr := IntToStr(ACoverageBase.GetPercentCovered());
+  PercentageStr := IntToStr(ACoverageStats.GetPercentCovered());
 
   Result := PercentageStr +
             '%' +
             StringOfChar(' ', 4 - Length(PercentageStr)) +
             '(' +
-            IntToStr(ACoverageBase.GetNumberOfCoveredLines()) +
+            IntToStr(ACoverageStats.GetNumberOfCoveredLines()) +
             '/' +
-            IntToStr(ACoverageBase.GetNumberOfLines()) + ')';
+            IntToStr(ACoverageStats.GetNumberOfLines()) + ')';
 end;
 
 procedure TXMLCoverageReport.AddPostAmble(const AOutFile: TextFile);
