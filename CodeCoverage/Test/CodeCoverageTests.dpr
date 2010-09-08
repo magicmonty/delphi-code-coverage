3804aecc99db9d5810e28484d805de83b831b0d6
christer.fahlgren <christer.fahlgren@b99ad5fc-3a95-0f71-d2d7-ed0b564f6493>
2010-09-08 18:55:08 +0000

    Catching exception during test and log it
    git-svn-id: http://delphi-code-coverage.googlecode.com/svn/trunk@44 b99ad5fc-3a95-0f71-d2d7-ed0b564f6493

--- CodeCoverage/Test/CodeCoverageTests.dpr
+++ CodeCoverage/Test/CodeCoverageTests.dpr
@@ -1,20 +1,21 @@
 program CodeCoverageTests;
 {
 
-  Delphi DUnit Test Project
-  -------------------------
-  This project contains the DUnit test framework and the GUI/Console test runners.
-  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
-  to use the console test runner.  Otherwise the GUI test runner will be used by
-  default.
+ Delphi DUnit Test Project
+ -------------------------
+ This project contains the DUnit test framework and the GUI/Console test runners.
+ Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+ to use the console test runner.  Otherwise the GUI test runner will be used by
+ default.
 
 }
-
 {$IFDEF CONSOLE_TESTRUNNER}
 {$APPTYPE CONSOLE}
 {$ENDIF}
 
 uses
+  Classes,
+  SysUtils,
   Forms,
   TestFramework,
   GUITestRunner,
@@ -22,16 +23,20 @@ uses
   TestConfiguration in 'TestConfiguration.pas',
   CoverageConfiguration in '..\CoverageConfiguration.pas',
   I_CoverageConfiguration in '..\I_CoverageConfiguration.pas',
-  I_ParameterProvider in '..\I_ParameterProvider.pas';
-
+  I_ParameterProvider in '..\I_ParameterProvider.pas', SysUtils;
 {$R *.RES}
 
 begin
-  Application.Initialize;
-  if IsConsole then
-    with TextTestRunner.RunRegisteredTests do
-      Free
-  else
-    GUITestRunner.RunRegisteredTests;
-end.
+  try
+    Application.Initialize;
+    if IsConsole then
+      with TextTestRunner.RunRegisteredTests do
+        Free
+      else
+        GUITestRunner.RunRegisteredTests;
+  except
+    on E: Exception do
+      writeln('Exception caught:' + E.ToString);
+  end;
 
+end.
